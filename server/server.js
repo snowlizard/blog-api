29c86a50772597a54b24c9370598840afcf2ea51
julian <illegal_burrito@live.com>
2022-03-31 14:59:03 -0700

    added route for user

--- server/server.js
+++ server/server.js
@@ -3,8 +3,7 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/my-blog',
-    {useMongoClient: true});
+mongoose.connect('mongodb://localhost/my-blog');
 mongoose.Promise = Promise;
 
 const app = express();
@@ -12,6 +11,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(morgan('dev'));
+
+app.use('/api/users', require('./routes/users'));
 
 app.get('/', (req, res) => {
     res.send('hello blog');
